271cf028d62141e8a111af093f77f12dbd875d48
Yabir Benchakhtir <yabirg@protonmail.com>
2025-01-27 20:55:36 +0100

    New assets mappings 01/2025

---
+++ updates/35/asset_collections_updates.sql
@@ -0,0 +1,4 @@
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (349, "Bold Stablecoin", "Bold", "eip155:1/erc20:0xb01dd87B29d187F3E3a4Bf6cdAebfb97F3D9aB98");
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (350, "SPX6900", "SPX", "eip155:1/erc20:0xE0f63A424a4439cBE457D80E4f4b51aD25b2c56C");
+*
